f9f95ff5fbf1e27d7768fab7d22957c3bb75110b
Jeremy Waugh <jwaugh@gmail.com>
2022-01-17 18:54:49 -0500

    add exception class

---
+++ src/Exception/IdealOctoTelegramException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace IdealOctoTelegram\Exception;
+
+/**
+ * Generic exception class for our app. In reality we would 
+ *    create multiple spicific exceptions.
+ */
+class IdealOctoTelegramException extends \Exception {}
